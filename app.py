49e903f9f78d8092f6fafa13b09f212b8ea92849
chandana-21 <chandanakuntala21@gmail.com>
2021-12-06 21:40:15 +0530

    code pushed

---
+++ app.py
@@ -0,0 +1,21 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write(
+    """
+
+    # Stock Price Python App
+
+    Stock closig price and volume of Google are shown below!
+
+    """
+)
+
+tickerSymbol = 'AAPL'
+
+tickerData = yf.Ticker(tickerSymbol)
+
+tickerDf = tickerData.history(period='1d', start='2011-12-1', end='2021-12-1')
+
+tickerDf
